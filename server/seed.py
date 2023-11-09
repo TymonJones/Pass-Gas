be153628297f80cdcf65a4039ab7cc2b936062be
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-11-09 07:02:58 -0800

    completed backend tests/setup

--- server/seed.py
+++ server/seed.py
@@ -1,45 +1,30 @@
+from faker import Faker
 from app import app, db
 from models import GasStation
-from faker import Faker 
-# Sample list of gas stations
-stations_data = [
-    {
-        "name": "Quality Fuels",
-        "address": "1234 Sunset Blvd, Los Angeles, CA",
-        "latitude": 34.052235,
-        "longitude": -118.243683,
-        "gas_price": 3.89
-    },
-    {
-        "name": "Eco Petro",
-        "address": "5678 Hollywood Blvd, Los Angeles, CA",
-        "latitude": 34.101235,
-        "longitude": -118.326683,
-        "gas_price": 4.09
-    },
-    
-]
 
-def seed_database():
+fake = Faker()
+
+def seed_database(num_entries=10):
     # Clear existing data
     db.drop_all()
     db.create_all()
-    
+
     # Create new entries
-    for station_data in stations_data:
+    for _ in range(num_entries):
         station = GasStation(
-            name=station_data["name"],
-            address=station_data["address"],
-            latitude=station_data["latitude"],
-            longitude=station_data["longitude"],
-            gas_price=station_data["gas_price"]
+            name=fake.company(),
+            address=fake.address(),
+            latitude=fake.latitude(),
+            longitude=fake.longitude(),
+            gas_price=round(fake.random_number(digits=2) + fake.random_number(digits=2) / 100, 2)
         )
         db.session.add(station)
-    
+
     # Commit changes
     db.session.commit()
 
 if __name__ == "__main__":
-    with app.app_context():  # Push an application context for the database interactions
-        seed_database()
+    with app.app_context():
+        seed_database(20)  # Seed the database with 20 random gas stations
+
 
